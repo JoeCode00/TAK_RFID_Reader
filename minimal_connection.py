e987f02d1e1643708c8c196d27ebdd91a236c724
JoeCode00 <joe.t.hill00@gmail.com>
2025-11-13 10:44:14 -0500

    example

--- minimal_connection.py
+++ minimal_connection.py
@@ -1,22 +1,94 @@
-from pytak import Client
-from pytak.cot import Event, Point, Detail
+#!/usr/bin/env python3
 
-client = Client(host="172.20.10.6", port=8087, callsign="PYTAK_USER")
-client.connect()
+import asyncio
+import xml.etree.ElementTree as ET
+import pytak
 
-event = Event(
-    uid="PYTAK-001",
-    type="a-u-V",
-    time="2025-11-13T10:15:00Z",
-    start="2025-11-13T10:15:00Z",
-    stale="2025-11-13T10:45:00Z",
-    point=Point(lat=29.6516, lon=-82.3248, hae=10),
-    detail=Detail(
-        contact=dict(callsign="PYTAK_USER"),
-        precisionlocation=dict(geopointsrc="GPS", altsrc="Barometric"),
-        track=dict(speed=12.3, course=85.0)
+from configparser import ConfigParser
+
+
+def gen_cot():
+    """Generate CoT Event."""
+    root = ET.Element("event")
+    root.set("version", "2.0")
+    root.set("type", "a-h-A-M-A")  # insert your type of marker
+    root.set("uid", "name_your_marker")
+    root.set("how", "m-g")
+    root.set("time", pytak.cot_time())
+    root.set("start", pytak.cot_time())
+    root.set(
+        "stale", pytak.cot_time(60)
+    )  # time difference in seconds from 'start' when stale initiates
+
+    pt_attr = {
+        "lat": "40.781789",  # set your lat (this loc points to Central Park NY)
+        "lon": "-73.968698",  # set your long (this loc points to Central Park NY)
+        "hae": "0",
+        "ce": "10",
+        "le": "10",
+    }
+
+    ET.SubElement(root, "point", attrib=pt_attr)
+
+    return ET.tostring(root)
+
+
+class MySender(pytak.QueueWorker):
+    """
+    Defines how you process or generate your Cursor-On-Target Events.
+    From there it adds the COT Events to a queue for TX to a COT_URL.
+    """
+
+    async def handle_data(self, data):
+        """Handle pre-CoT data, serialize to CoT Event, then puts on queue."""
+        event = data
+        await self.put_queue(event)
+
+    async def run(self):
+        """Run the loop for processing or generating pre-CoT data."""
+        while True:
+            data = gen_cot()
+            self._logger.info("Sending:\n%s\n", data.decode())
+            await self.handle_data(data)
+            await asyncio.sleep(5)
+
+
+class MyReceiver(pytak.QueueWorker):
+    """Defines how you will handle events from RX Queue."""
+
+    async def handle_data(self, data):
+        """Handle data from the receive queue."""
+        self._logger.info("Received:\n%s\n", data.decode())
+
+    async def run(self):
+        """Read from the receive queue, put data onto handler."""
+        while True:
+            data = (
+                await self.queue.get()
+            )  # this is how we get the received CoT from rx_queue
+            await self.handle_data(data)
+
+
+async def main():
+    """Main definition of your program, sets config params and
+    adds your serializer to the asyncio task list.
+    """
+    config = ConfigParser()
+    config["mycottool"] = {"COT_URL": "tcp://takserver.example.com:8087"}
+    config = config["mycottool"]
+
+    # Initializes worker queues and tasks.
+    clitool = pytak.CLITool(config)
+    await clitool.setup()
+
+    # Add your serializer to the asyncio task list.
+    clitool.add_tasks(
+        set([MySender(clitool.tx_queue, config), MyReceiver(clitool.rx_queue, config)])
     )
-)
 
-client.send_coa(event)   # transmit the CoT XML to the OpenTAK server
-client.run_forever()
+    # Start all tasks.
+    await clitool.run()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
