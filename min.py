635d3f159cfff74f088d4e22feaa608f03951e9d
JoeCode00 <joe.t.hill00@gmail.com>
2025-11-14 00:15:05 -0500

    push

--- min.py
+++ min.py
@@ -1,11 +1,18 @@
-from atakcots import CotConfig, push_cot
+from atakcots import CotConfig, CotServer
 
+cot_config = CotConfig(
+    uid="Message",
+    latitude=40.74931973338903,
+    longitude=-73.96791282024928,
+    attachment_paths="sandeot.png"
+)
 
-if __name__ == "__main__":
-    cot_config = CotConfig(
-        uid="Message",
-        latitude=40.74931973338903,
-        longitude=-73.96791282024928
-    )
+server = CotServer("172.20.10.6", 8000)
+server.start()
 
-    push_cot(cot_config, "172.20.10.6")
+server.push_cot(cot_config, "172.20.10.6")
+server.push_cot(cot_config, "172.20.10.6")
+server.push_cot(cot_config, "172.20.10.6")
+
+# stop when clients no longer need to fetch attachments
+server.stop()
