b8ed7f35b0d013ddc94f3c559b998ff42716bbb0
JoeCode00 <joe.t.hill00@gmail.com>
2025-11-13 23:49:05 -0500

    min

---
+++ min.py
@@ -0,0 +1,27 @@
+import asyncio
+import pytak
+
+
+async def main():
+    """Connect to TAK server over TCP"""
+    # Configure the TAK client
+    config = {
+        'COT_URL': 'tcp://172.20.10.6:8087',  # TAK server TCP endpoint
+    }
+    
+    # Create a TAK client
+    client = pytak.TXWorker(
+        event_queue=asyncio.Queue(),
+        url=config['COT_URL']
+    )
+    
+    try:
+        await client.run()
+    except KeyboardInterrupt:
+        print("Shutting down...")
+    finally:
+        await client.stop()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
