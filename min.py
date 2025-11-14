93b14289b01ce57f458104709595969ff9656961
JoeCode00 <joe.t.hill00@gmail.com>
2025-11-13 23:54:32 -0500

    config parser

--- min.py
+++ min.py
@@ -1,15 +1,21 @@
 import asyncio
+import configparser
 import pytak
 
 
 async def main():
     """Connect to TAK server over TCP"""
     # Configure the TAK client
-    config = pytak.Config()
-    config.COT_URL = 'tcp://172.20.10.6:8087'  # TAK server TCP endpoint
+    config = configparser.ConfigParser()
+    config['DEFAULT'] = {
+        'COT_URL': 'tcp://172.20.10.6:8087'
+    }
     
-    # Create event queue
-    clitool = pytak.CLITool(config)
+    # Create event queue and workers
+    tx_queue = asyncio.Queue()
+    rx_queue = asyncio.Queue()
+    
+    clitool = pytak.CLITool(config, tx_queue, rx_queue)
     await clitool.setup()
     
     # Create and run the client
