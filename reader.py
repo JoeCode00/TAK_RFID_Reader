65e4fb6b13aee5a3f02f2e144c16d40834589cd4
JoeCode00 <30753256+JoeCode00@users.noreply.github.com>
2025-11-16 15:18:25 +0000

    handling multiple markers

--- reader.py
+++ reader.py
@@ -7,5 +7,9 @@ s.headers['X-XSRF-TOKEN'] = csrf_token
 s.headers['Referer'] = address
 
 r = s.post(f"{address}/api/login", json={'username': 'reader', 'password': 'reader'}, verify=False)
-r = s.get(f"{address}/api/markers", json={}, verify=False)
-print(r.text)
+response_markers = s.get(f"{address}/api/markers", json={}, verify=False)
+for marker in response_markers.json()['results']:
+    callsign = marker['callsign']
+    lattitude = marker['point']['latitude']
+    longitude = marker['point']['longitude']
+    print(callsign, lattitude, longitude)
