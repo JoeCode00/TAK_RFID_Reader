bfd8c2a64a933d7db1f87d3f60f532e5b258177c
JoeCode00 <30753256+JoeCode00@users.noreply.github.com>
2025-11-16 16:35:05 +0000

    .

--- reader.py
+++ reader.py
@@ -39,7 +39,7 @@ try:
 
         data = ser.readall()
         cleaned = data.replace(b'\nU\r\n', b'').replace(b'\nX\r\n', b'')
-        if cleaned not in [b'', b'\r', b'\n', b'U', b'X', b'\r\n', b'\nU', b'\nX', b'U\r\n', b'X\r\n', b'X\r', b'U\r']:
+        if cleaned not in [b'', b'\r', b'\n', b'U', b'X', b'\r\n', b'\nU', b'\nX', b'U\r\n', b'X\r\n', b'X\r', b'U\r', b'\nU\r', b'\nX\r']:
             print(cleaned)
         # # --- Receiving Bytes ---
         # # Read up to 100 bytes (or until timeout)
