4200ee24375c423cbaa0716a24c8cbdc69eb28bf
JoeCode00 <30753256+JoeCode00@users.noreply.github.com>
2025-11-16 15:58:04 +0000

    .

--- reader.py
+++ reader.py
@@ -39,7 +39,8 @@ try:
 
         received_line = ser.readline()
         string_data = received_line.decode('utf-8').strip()  # Decode bytes to string and strip whitespace
-        print(string_data)
+        if len(string_data)>2:
+            print(string_data)
 
         # split = string_data.split(",")
         # for item in split:
